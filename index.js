4b90bf6be4e02b126de725b58582a4fd9e0fe145
Christine <christinekhekama99@gmail.com>
2023-06-16 12:34:37 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+const square = (number) => number * number;
+const divide = () => {
+    const result = 2000 / 100;
+    return result;
+  };
